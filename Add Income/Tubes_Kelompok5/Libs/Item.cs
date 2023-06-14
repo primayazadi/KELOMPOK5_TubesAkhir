565a8691d400ca96aa39b8d9ca3d04115de850ee
nadhifaaffiyan <127294844+nadhifaaffiyan@users.noreply.github.com>
2023-06-14 21:29:26 +0700

    Add Income

---
+++ Add Income/Tubes_Kelompok5/Libs/Item.cs
@@ -0,0 +1,33 @@
+﻿namespace Tubes_Kelompok5.Libs
+{
+    public class Item
+    {
+        public string tanggal
+        {
+            get; set;
+        }
+
+        public string nama
+        {
+            get; set;
+        }
+        public string harga
+        {
+            get; set;
+        }
+        public string jumlah
+        {
+            get; set;
+        }
+        public string username
+        {
+            get; set;
+        }
+
+        public string total
+        {
+            get; set;
+        }
+    }
+}
+
