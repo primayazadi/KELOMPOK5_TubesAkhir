fc0e1c4bea75030a958e524d3b0b47636b3b3a6e
nadhifaaffiyan <127294844+nadhifaaffiyan@users.noreply.github.com>
2023-06-15 16:59:16 +0700

    Add Income

---
+++ Tubes_Kelompok5/Program.cs
@@ -0,0 +1,31 @@
+namespace Tubes_Kelompok5
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            /*
+            FormAddExpend formExpend = new FormAddExpend();
+            formExpend.userName = "Shielda";
+            formExpend.ShowDialog();
+            */
+            
+            FormAddIncome formAddIncome = new FormAddIncome();
+            formAddIncome.userName = "Nadhifa";
+            formAddIncome.ShowDialog();
+
+
+
+            
+
+        }
+    }
+}
