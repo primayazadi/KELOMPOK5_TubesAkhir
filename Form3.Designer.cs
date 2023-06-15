82deb1aac0088a16315662a59b1c1c9a4f223f6b
Ephymeral Aduare <109929578+rwudboy@users.noreply.github.com>
2023-06-15 23:52:53 +0700

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace tubesAKHIR_prima
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.Cyan;
+            label1.Location = new Point(274, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(240, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Laporan Keuangan";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(395, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.Cyan;
+            label2.Location = new Point(274, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Total Pemasukan :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.Cyan;
+            label3.Location = new Point(274, 142);
+            label3.Name = "label3";
+            label3.Size = new Size(107, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Total Pengeluaran :";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(395, 134);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.Cyan;
+            label4.Location = new Point(274, 171);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Net Income :";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(395, 163);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 5;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Black;
+            button1.ForeColor = Color.Cyan;
+            button1.Location = new Point(349, 215);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 7;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ControlDarkDark;
+            Name = "Form3";
+            Text = "Form3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tubesAKHIR_prima.Libs;
+
+namespace tubesAKHIR_prima
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            //income
+            string jsonFilePath = "Income-data.json";
+            //expand
+            string jsonFilePath2 = "Expend-data.json";
+
+
+            string jsonContent = File.ReadAllText(jsonFilePath);
+            string jsonContent2 = File.ReadAllText(jsonFilePath2);
+            var trx = JsonConvert.DeserializeObject<Transaction>(jsonContent);
+            var trx2 = JsonConvert.DeserializeObject<Transaction>(jsonContent2);
+            int income = Convert.ToInt32(trx.trxTotal);
+            int expanse = Convert.ToInt32(trx2.trxTotal);
+            int total = income - expanse;
+            textBox1.Text = "rp." + income.ToString("N");
+            textBox2.Text = "rp." + expanse.ToString("N");
+            textBox3.Text = "rp." + total.ToString("N");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dashboard k = new Dashboard();
+            k.Show();
+            this.Hide();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
